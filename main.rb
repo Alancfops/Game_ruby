e80eb0a211772d2bf224ac242b99225954e07e4b
Alan Cristian <142458955+Alancfops@users.noreply.github.com>
2024-08-12 18:29:41 -0300

    Fix: bug when creating the GuessNumber class object

--- main.rb
+++ main.rb
@@ -3,7 +3,7 @@ require_relative 'classes'
 game = GuessNumber.new
 
 until game.win
-  puts "Digite um numero: "
+  puts "Digite um numero de 1 a 15: "
   number = gets.to_i 
   
   puts game.try_guess(number)
